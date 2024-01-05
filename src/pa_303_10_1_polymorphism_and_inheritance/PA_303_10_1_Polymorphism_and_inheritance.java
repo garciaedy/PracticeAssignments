52f228bf10c6921422effda9beb578709271fd2f
garciaedy <edygarcia97@gmail.com>
2024-01-05 00:30:43 -0500

    adding prac assignments

---
+++ src/pa_303_10_1_polymorphism_and_inheritance/PA_303_10_1_Polymorphism_and_inheritance.java
@@ -0,0 +1,33 @@
+package pa_303_10_1_polymorphism_and_inheritance;
+
+public class PA_303_10_1_Polymorphism_and_inheritance {
+
+	public static void main(String[] args) {
+		
+		/* */
+		
+		/* */
+		
+		/* */
+		
+		/* */
+		/* */
+		
+		/* */
+		
+		/* */
+		
+		/* */
+		
+		/* */
+		
+		/* */
+		
+		/* */
+		
+		/* */
+		
+
+	}
+
+}
---
+++ src/pa_303_2_2__scanner_Class/PA_303_2_2__Scanner_Class.java
@@ -0,0 +1,31 @@
+package pa_303_2_2__scanner_Class;
+
+public class PA_303_2_2__Scanner_Class {
+
+	public static void main(String[] args) {
+		
+		/* */
+		
+		/* */
+		
+		/* */
+		
+		/* */
+		/* */
+		
+		/* */
+		
+		/* */
+		
+		/* */
+		
+		/* */
+		
+		/* */
+		
+		/* */
+		
+		/* */
+	}
+
+}
