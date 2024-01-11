490399b5a596f69531beb629480ef255333adef7
garciaedy <edygarcia97@gmail.com>
2024-01-11 14:52:17 -0500

    adding code

---
+++ src/func_interface_mylabs_poc/FuncInterface3.java
@@ -0,0 +1,29 @@
+package func_interface_mylabs_poc;
+
+// Java program to demonstrate lambda expressions to
+// implement a user defined functional interface.
+
+// Single Abstract Method (SAM) types or interfaces
+
+// The major benefit of java 8 functional interfaces is that we
+// can use lambda expressions to instantiate them and avoid
+// using bulky anonymous class implementation.
+
+//  WHAT YOU NEED TODO: Implement the FunctionalInterface  Square
+public class FuncInterface3 {
+    public static void main(String args[])
+    {
+        int a = 5;
+
+        // lambda expression to define the calculate method
+        Square s = (int x) -> x * x;
+
+        // parameter passed and return type must be
+        // same as defined in the prototype
+        int ans = s.calculate(a);
+        System.out.println(ans);
+    }
+
+
+}
+
