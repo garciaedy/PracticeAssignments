31ecde42d9386ac4eaf8ca8f8af15ca289cc3ae8
garciaedy <edygarcia97@gmail.com>
2024-01-11 15:05:45 -0500

    added code

--- src/func_interface_mylabs_poc/FunctionJavaUtil3.java
+++ src/func_interface_mylabs_poc/FunctionJavaUtil3.java
@@ -1,8 +1,8 @@
 package func_interface_mylabs_poc;
 
 // Java Program to illustrate compose() method
-
 //Importing Function interface
+
 import java.util.function.Function;
 
 //Main class
@@ -16,7 +16,11 @@ public class FunctionJavaUtil3 {
         Function<Integer, Double> half = a -> a / 2.0;
 
         // INSERT CODE TO TRIPLE THE VALUE OF THE HALF FUNCTION
+        // Function to triple the value
+        Function<Double, Double> triple = a -> a*3;
 
+        // Composing the functions to first half and then triple the result
+        Function<Integer, Double> halfThenTriple = half.andThen(triple);
 
         // Applying the function to get the result
         System.out.println(half.apply(5));
