292be46cbdbf8a3f9ac1cce1ef5851cedda884e5
garciaedy <edygarcia97@gmail.com>
2024-01-11 14:58:10 -0500

    adding starter code

--- src/func_interface_mylabs_poc/FunctionJavaUtil3.java
+++ src/func_interface_mylabs_poc/FunctionJavaUtil3.java
@@ -1,2 +1,24 @@
-package func_interface_mylabs_poc;public class FunctionJavaUtil3 {
+package func_interface_mylabs_poc;
+
+// Java Program to illustrate compose() method
+
+//Importing Function interface
+import java.util.function.Function;
+
+//Main class
+
+public class FunctionJavaUtil3 {
+    // Main driver method
+    public static void main(String args[])
+    {
+        // Function which takes in a number and
+        // returns half of it
+        Function<Integer, Double> half = a -> a / 2.0;
+
+        // INSERT CODE TO TRIPLE THE VALUE OF THE HALF FUNCTION
+
+
+        // Applying the function to get the result
+        System.out.println(half.apply(5));
+    }
 }
